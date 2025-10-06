aed1fcd46f68d0f6f8f38173269c709f3a648ff7
mdew-05 <matthijsdewolff@gmail.com>
2025-10-06 13:26:52 +0200

    Update laadpalensteamlit.py

--- laadpalensteamlit.py
+++ laadpalensteamlit.py
@@ -7,16 +7,30 @@ import geopandas as gpd
 
 st.set_page_config(page_title="Laadpalen en Elektrisch vervoer", layout="wide")
 
-#Api Reader
-response = requests.get("https://api.openchargemap.io/v3/poi/?output=json&countrycode=NL&maxresults=100&compact=true&verbose=false&key=2960318e-86ae-49e0-82b1-3c8bc6790b41")
-responsejson  = response.json()
-Laadpalen = pd.json_normalize(responsejson)
-df4 = pd.json_normalize(Laadpalen.Connections)
-df5 = pd.json_normalize(df4[0])
-Laadpalen = pd.concat([Laadpalen, df5], axis=1)
-columns_to_exclude=["Amps", "Voltage", "AddressInfo.StateOrProvince", "NumberOfPoints", "UsageCost", "UUID", "DataProviderID", "Reference", "Connections", "AddressInfo.DistanceUnit", "AddressInfo.AddressLine2", "AddressInfo.ContactTelephone1", "AddressInfo.RelatedURL", "DataProvidersReference", "IsRecentlyVerified", "DataQualityLevel", "AddressInfo.CountryID", "SubmissionStatusTypeID"]
-Laadpalen1 = Laadpalen.drop(columns_to_exclude, axis=1)
+#Api Reader 
 
+response = requests.get("https://api.openchargemap.io/v3/poi/?output=json&countrycode=NL&maxresults=6000&compact=true&verbose=false&key=2960318e-86ae-49e0-82b1-3c8bc6790b41") 
+responsejson = response.json() 
+Laadpalen = pd.json_normalize(responsejson) 
+df4 = pd.json_normalize(Laadpalen.Connections) 
+df5 = pd.json_normalize(df4[0]) 
+Laadpalen = pd.concat([Laadpalen, df5], axis=1)
+columns_to_drop = ["Amps", "Voltage", "AddressInfo.StateOrProvince", "NumberOfPoints", "UsageCost", "UUID", "DataProviderID", "Reference", "Connections", "AddressInfo.DistanceUnit", "AddressInfo.AddressLine2", "AddressInfo.ContactTelephone1", "AddressInfo.RelatedURL", "DataProvidersReference", "IsRecentlyVerified", "DataQualityLevel", "AddressInfo.CountryID", "SubmissionStatusTypeID"]
+Laadpalen1 = Laadpalen.drop(columns_to_drop, axis=1, inplace=True) 
+geometry = [Point(a) for a in zip(Laadpalen["AddressInfo.Longitude"], Laadpalen["AddressInfo.Latitude"])]
+Laadpalen1 = gpd.GeoDataFrame(Laadpalen, geometry=geometry, crs="EPSG:4326")
+
+#map 
+m = folium.Map(location=[52.1, 5.3], zoom_start=8)
+marker_cluster = MarkerCluster().add_to(m)
+for _, row in Laadpalen1.iterrows():
+    folium.Marker(
+        location=[row["AddressInfo.Latitude"], row["AddressInfo.Longitude"]],
+        popup=row["AddressInfo.Title"] if "AddressInfo.Title" in row else "Charging Station"
+    ).add_to(marker_cluster)
+m
+
+#streamlit
 st.sidebar.title("Controls")
 
 year_range = st.sidebar.slider("Select Year Range", 2010, 2025, (2010, 2025)) #moet nog de juiste daterange worden
@@ -29,6 +43,7 @@ tab1, tab2, tab3 = st.tabs([
    "Oplaad data",
    "Laadpalen map"
 ])
+
 
 
 
