63f2a8575a3d0c08285482315774d169dff97c0b
Nicknameisnick <wessel.arend@gmail.com>
2025-10-06 14:27:21 +0200

    Update laadpalensteamlit.py

--- laadpalensteamlit.py
+++ laadpalensteamlit.py
@@ -1,16 +1,37 @@
+import requests
 import pandas as pd
 import streamlit as st
 import plotly.express as px
+import geopandas as gpd
+from shapely.geometry import Point
+import folium
+from folium.plugins import MarkerCluster
+from streamlit_folium import st_folium
 
 st.set_page_config(page_title="Laadpalen en Elektrisch vervoer", layout="wide")
 
 # -------------------------------
-# Sidebar Controls
+# API Reader - Laadpalen Data
 # -------------------------------
-st.sidebar.title("Controls")
-year_range = st.sidebar.slider("Select Year Range", 2010, 2025, (2010, 2025))
-show_lines = st.sidebar.checkbox("Show Lines", value=True)
-show_points = st.sidebar.checkbox("Show Points", value=True)
+response = requests.get("https://api.openchargemap.io/v3/poi/?output=json&countrycode=NL&maxresults=6000&compact=true&verbose=false&key=2960318e-86ae-49e0-82b1-3c8bc6790b41") 
+responsejson = response.json() 
+Laadpalen = pd.json_normalize(responsejson) 
+df4 = pd.json_normalize(Laadpalen.Connections) 
+df5 = pd.json_normalize(df4[0]) 
+Laadpalen = pd.concat([Laadpalen, df5], axis=1)
+
+columns_to_drop = [
+    "Amps", "Voltage", "AddressInfo.StateOrProvince", "NumberOfPoints", "UsageCost", "UUID", 
+    "DataProviderID", "Reference", "Connections", "AddressInfo.DistanceUnit", 
+    "AddressInfo.AddressLine2", "AddressInfo.ContactTelephone1", "AddressInfo.RelatedURL", 
+    "DataProvidersReference", "IsRecentlyVerified", "DataQualityLevel", "AddressInfo.CountryID", 
+    "SubmissionStatusTypeID"
+]
+Laadpalen.drop(columns_to_drop, axis=1, inplace=True)
+
+geometry = [Point(a) for a in zip(Laadpalen["AddressInfo.Longitude"], Laadpalen["AddressInfo.Latitude"])]
+Laadpalen1 = gpd.GeoDataFrame(Laadpalen, geometry=geometry, crs="EPSG:4326")
+
 
 # -------------------------------
 # Functions
@@ -97,12 +118,23 @@ def auto_per_maand(data_cars):
     with col2:
         st.plotly_chart(fig_hist, use_container_width=True)
 
+def build_map():
+    m = folium.Map(location=[52.1, 5.3], zoom_start=8)
+    marker_cluster = MarkerCluster().add_to(m)
+    for _, row in Laadpalen1.iterrows():
+        folium.Marker(
+            location=[row["AddressInfo.Latitude"], row["AddressInfo.Longitude"]],
+            popup=row.get("AddressInfo.Title", "Charging Station")
+        ).add_to(marker_cluster)
+    return m
+
 # -------------------------------
 # Tabs
 # -------------------------------
-tab1, tab2 = st.tabs([ 
+tab1, tab2, tab3 = st.tabs([ 
    "Voertuigverdeling over de tijd", 
-   "Oplaad data"
+   "Oplaad data",
+   "Laadpalen map"
 ])
 
 # Tab 1: Voertuigverdeling
@@ -111,6 +143,7 @@ with tab1:
     data_cars['brandstof'] = data_cars['handelsbenaming'].apply(bepaal_brandstof)
     auto_per_maand(data_cars)
 
-# Tab 2: placeholder
-with tab2:
-    st.write("Hier komt later oplaad data 📊")
+# Tab 3: Laadpalen map
+with tab3:
+    m = build_map()  # build first
+    st_folium(m, width=800, height=600)  # render after
