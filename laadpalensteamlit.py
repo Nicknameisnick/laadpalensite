8b5d95f250f372658ac7c16baf759febce64699b
mdew-05 <matthijsdewolff@gmail.com>
2025-10-06 12:55:39 +0200

    Update laadpalensteamlit.py

--- laadpalensteamlit.py
+++ laadpalensteamlit.py
@@ -6,8 +6,15 @@ import numpy as np
 
 st.set_page_config(page_title="Laadpalen en Elektrisch vervoer", layout="wide")
 
+#Api Reader
 response = requests.get("https://api.openchargemap.io/v3/poi/?output=json&countrycode=NL&maxresults=100&compact=true&verbose=false&key=2960318e-86ae-49e0-82b1-3c8bc6790b41")
 responsejson  = response.json()
+Laadpalen = pd.json_normalize(responsejson)
+df4 = pd.json_normalize(Laadpalen.Connections)
+df5 = pd.json_normalize(df4[0])
+Laadpalen = pd.concat([Laadpalen, df5], axis=1)
+columns_to_exclude=["Amps", "Voltage", "AddressInfo.StateOrProvince", "NumberOfPoints", "UsageCost", "UUID", "DataProviderID", "Reference", "Connections", "AddressInfo.DistanceUnit", "AddressInfo.AddressLine2", "AddressInfo.ContactTelephone1", "AddressInfo.RelatedURL", "DataProvidersReference", "IsRecentlyVerified", "DataQualityLevel", "AddressInfo.CountryID", "SubmissionStatusTypeID"]
+Laadpalen1 = Laadpalen.drop(columns_to_exclude, axis=1)
 
 st.sidebar.title("Controls")
 
@@ -21,6 +28,7 @@ tab1, tab2, tab3 = st.tabs([
    "Oplaad data",
    "Laadpalen map"
 ])
+
 
 
 
