b8480798edf6bac55699fdd483521c00f08a897b
Nicknameisnick <wessel.arend@gmail.com>
2025-10-07 01:47:13 +0200

    Update laadpalensteamlit.py

--- laadpalensteamlit.py
+++ laadpalensteamlit.py
@@ -10,24 +10,7 @@ from streamlit_folium import st_folium
 
 st.set_page_config(page_title="Laadpalen en Elektrisch vervoer", layout="wide")
 
-# -------------------------------
-# API Reader - Laadpalen Data
-# -------------------------------
-response = requests.get("https://api.openchargemap.io/v3/poi/?output=json&countrycode=NL&maxresults=6000&compact=true&verbose=false&key=2960318e-86ae-49e0-82b1-3c8bc6790b41") 
-responsejson = response.json() 
-Laadpalen = pd.json_normalize(responsejson) 
-df4 = pd.json_normalize(Laadpalen.Connections) 
-df5 = pd.json_normalize(df4[0]) 
-Laadpalen = pd.concat([Laadpalen, df5], axis=1)
-
-columns_to_drop = [
-    "Amps", "Voltage", "AddressInfo.StateOrProvince", "NumberOfPoints", "UsageCost", "UUID", 
-    "DataProviderID", "Reference", "Connections", "AddressInfo.DistanceUnit", 
-    "AddressInfo.AddressLine2", "AddressInfo.ContactTelephone1", "AddressInfo.RelatedURL", 
-    "DataProvidersReference", "IsRecentlyVerified", "DataQualityLevel", "AddressInfo.CountryID", 
-    "SubmissionStatusTypeID"
-]
-Laadpalen.drop(columns_to_drop, axis=1, inplace=True)
+laadpalen = pd.read_csv('laadpalen_api_data')
 
 geometry = [Point(a) for a in zip(Laadpalen["AddressInfo.Longitude"], Laadpalen["AddressInfo.Latitude"])]
 Laadpalen1 = gpd.GeoDataFrame(Laadpalen, geometry=geometry, crs="EPSG:4326")
@@ -138,6 +121,7 @@ with tab1:
 with tab3:
     m = build_map()
     st_folium(m, width=800, height=600)
+
 
 
 
