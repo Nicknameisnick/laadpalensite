2ec1fb3a4cb51df4bb98594549a04904afe4e0da
mdew-05 <matthijsdewolff@gmail.com>
2025-10-06 12:26:49 +0200

    Update laadpalensteamlit.py

--- laadpalensteamlit.py
+++ laadpalensteamlit.py
@@ -3,8 +3,9 @@ import pandas as pd
 
 
 
-response = requests.get("https://api.openchargemap.io/v3")
+response = requests.get("https://api.openchargemap.io/v3?output=json&countrycode=NL&maxresults=100&compact=true&verbose=false&key=2960318e-86ae-49e0-82b1-3c8bc6790b41")
 responsejson  = response.json()
+
 
 
 
